5fa1e09adb344ac395c7de869a138d9710c007df
Vladyslav <vladislav2002vladislav2002@gmail.com>
2023-09-30 20:29:12 +0300

    task11

---
+++ src/index.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const KEY = '39758171-454faa2ce1768c54f925ff819';
+
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+searchForm.addEventListener('submit', onSearchForm);
+
+function renderGallery(images) {
+  if (!gallery) {
+    return;
+  }
+
+  const markup = images
+    .map(image => {
+      const { id, largeImageURL, webformatURL, tags, likes, views, comments, downloads, } = image;
+      return `
+        <a class="gallery__link" href="${largeImageURL}">
+          <div class="gallery-item" id="${id}">
+            <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes</b>${likes}</p>
+              <p class="info-item"><b>Views</b>${views}</p>
+              <p class="info-item"><b>Comments</b>${comments}</p>
+              <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+          </div>
+        </a>
+      `;
+    })
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.elements.searchQuery.value.trim();
+  gallery.innerHTML = '';
+
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.',
+    );
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
+
+function onloadMore() {
+  page += 1;
+  simpleLightBox.destroy();
+
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results.",
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function checkIfEndOfPage() {
+  return (
+    window.innerHeight + window.pageYOffset >=
+    document.documentElement.scrollHeight
+  );
+}
+
+function showLoadMorePage() {
+  if (checkIfEndOfPage()) {
+    onloadMore();
+  }
+}
+
+window.addEventListener('scroll', showLoadMorePage);
+
+arrowTop.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+window.addEventListener('scroll', function () {
+  arrowTop.hidden = scrollY < document.documentElement.clientHeight;
+});
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    Notiflix.Notify.failure('Something went wrong. Please try again later.');
+    return Promise.reject(error);
+  },
+);
+
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response.data;
+}
+
+SmoothScroll({
+    animationTime: 800,
+    stepSize: 75,
+    accelerationDelta: 30,
+    accelerationMax: 2,
+    keyboardSupport: true,
+    arrowScroll: 50,
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+    touchpadSupport: true,
+  })
